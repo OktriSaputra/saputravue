6dc7bfc719e2efcbe355de171e009b34f28cbbd0
OktriSaputra <oktriwaw@gmail.com>
2020-07-10 00:44:33 +0700

    upload projek vue

---
+++ src/components/InstaSaputra.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- Instagram Section Begin -->
+    <div class="instagram-photo">
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-1.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-2.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-3.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-4.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-5.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+        <div class="insta-item set-bg" style="background-image: url('/img/new/insta-6.jpg')">
+            <div class="inside-text">
+                <i class="ti-instagram"></i>
+                <h5><a href="#">saputra_gallery</a></h5>
+            </div>
+        </div>
+    </div>
+    <!-- Instagram Section End -->
+</template>
+
+<script>
+export default {
+    name : 'InstaSaputra'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderSaputra /> 
+    <AwalSaputra />
+    <ListSaputra />
+    <InstaSaputra />
+    <PartnerSaputra />
+    <FooterSaputra />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSaputra from '@/components/HeaderSaputra.vue'
+import AwalSaputra from '@/components/AwalSaputra.vue'
+import ListSaputra from '@/components/ListSaputra.vue'
+import InstaSaputra from '@/components/InstaSaputra.vue'
+import PartnerSaputra from '@/components/PartnerSaputra.vue'
+import FooterSaputra from '@/components/FooterSaputra.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderSaputra,
+    AwalSaputra,
+    ListSaputra,
+    InstaSaputra,
+    PartnerSaputra,
+    FooterSaputra
+  }
+}
+</script>
